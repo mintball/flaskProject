b4f8fd7ed8cf119b3c6fa3bf9e7a0f4ab25aae82
mintball <bolza25120@gmail.com>
2022-08-07 13:18:15 +0700

    design index.html-->thai lang and login.css-->chang color background

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
 import pymysql
+from flask import Flask, render_template
+# Login
+from flask_login import LoginManager,login_required
 
+# create a flask application
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 # connect to database
 conn = pymysql.connect(host='localhost', user='root', password='', db='product_db')
@@ -9,18 +14,43 @@ if conn:
     print('Connected to database')
 
 
+# Homepage
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template('show.html')
+def Homepage():
+    return render_template('index.html')
 
 
-@app.route('/show')
-def show_product():
+@app.route('/about')
+def About():
+    return render_template('about.html'), 404
+
+
+@app.route('/contact')
+def Contact():
+    return render_template('contact.html'), 404
+
+
+# Login page
+@login_manager.user_loader
+def load_user(user_id):
     cur = conn.cursor()
-    cur.execute('SELECT * FROM product')
-    data = cur.fetchall()
-    return render_template('index.html', data=data)
+    cur.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+    user = cur.fetchone()
+    cur.close()
+    return user
+
+
+@app.route('/login')
+def Login():
+    return render_template('auth/login.html')
+
+
+# Dashboard page
+@app.route('/dashboard')
+@login_required
+def Dashboard():
+    return render_template('dashboard.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
